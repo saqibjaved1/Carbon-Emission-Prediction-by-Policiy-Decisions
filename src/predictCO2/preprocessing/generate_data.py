9a9a8e97381f4e3b5dfc8d23d8dc852323c3adc6
Tapan Sharma <01tapansharma@gmail.com>
2020-06-20 23:56:31 +0200

    Adding interface for training data

--- src/predictCO2/preprocessing/generate_data.py
+++ src/predictCO2/preprocessing/generate_data.py
@@ -17,3 +17,44 @@ class DataType(Enum):
     DICT = 1  # For type dictionary
     PANDAS_DF = 2  # For type pandas data frame
 
+
+class TrainDataInterface(object):
+    """
+    Interface representing training data. Defined below are abstract methods that are implemented by the respective
+    sub-classes for training.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get_features(self, in_form: DataType):
+        """
+        Return features as specified by the type
+        :param in_form: either Dictionary or Pandas Data frame
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_labels(self, in_form: DataType):
+        """
+        Return labels as specified by the type
+        :param: in_form: either Dictionary or Pandas Data frame
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_augmented_data(self, in_form: DataType):
+        """
+        Return augmented data combining features with labels
+        :param in_form: either Dictionary or Pandas Data frame
+        """
+        pass
+
+    @abc.abstractmethod
+    def save_data_frame_to_csv(self, location: str):
+        """
+        Save data frame as csv
+        :param location: Location on local file system
+        """
+        pass
+
+

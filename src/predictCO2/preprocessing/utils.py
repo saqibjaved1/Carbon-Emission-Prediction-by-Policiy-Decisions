755faae08997150f541170978289557ed1e1e0c0
Tapan Sharma <01tapansharma@gmail.com>
2020-06-27 22:37:18 +0200

    Adding functionality to handle panda Timestamps

--- src/predictCO2/preprocessing/utils.py
+++ src/predictCO2/preprocessing/utils.py
@@ -1,6 +1,9 @@
 """Created by: Tapan Sharma
 Date: 20/06/20
 """
+import Globals
+import pandas as pd
+import yaml
 
 
 def conform_date(date_str):
@@ -9,13 +12,29 @@ def conform_date(date_str):
     :param date_str: Date string from Global Carbon project (of form "DD/MM/YYYY")
     :return: date in form "YYYYMMDD"
     """
-    date_parts = date_str.split('/')
-    year = date_parts[2]
-    month = date_parts[1]
-    day = date_parts[0]
-    if len(day) == 1:
-        day = '0' + day
-    if len(month) == 1:
-        month = '0' + month
-    conformed_date = year + month + day
-    return conformed_date
+    if isinstance(date_str, pd.Timestamp):
+        return date_str.strftime(('%Y%m%d'))
+    else:
+        date_parts = None
+
+        if '/' in date_str:
+            date_parts = date_str.split('/')
+        if '-' in date_str:
+            date_parts = date_str.split('-')
+        if '.' in date_str:
+            date_parts = date_str.split('.')
+
+        if date_parts:
+            year = date_parts[2]
+            month = date_parts[1]
+            day = date_parts[0]
+            if len(day) == 1:
+                day = '0' + day
+            if len(month) == 1:
+                month = '0' + month
+            conformed_date = year + month + day
+            return conformed_date
+        else:
+            raise ValueError("Failed to conform date in YYYYMMDD format for received: {}".format(date_str))
+
+

3ce652f7a8a7a0f0ec3696434d3eaddb72d4eb5d
Tapan Sharma <01tapansharma@gmail.com>
2020-08-06 13:03:28 +0200

    Changed LSTM training and model evaluation script to use generalised dep learning model class.

--- tools/train_lstm2.py
+++ tools/train_lstm2.py
@@ -8,7 +8,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from predictCO2.models.lstm_2 import LSTM_2
+import time
+from predictCO2.models.deep_learning_model import DeepLearningModel
 from predictCO2.preprocessing import utils
 from predictCO2.preprocessing.generate_data import CountryPolicyCarbonData, PolicyCategory
 from sklearn.model_selection import TimeSeriesSplit
@@ -23,11 +24,12 @@ train_features = pd.DataFrame()
 train_labels = pd.DataFrame()
 test_features = pd.DataFrame()
 test_labels = pd.DataFrame()
-
+norm_data = training_config['training']['normalize']
 # Collect data
 for country in countries:
     countryPolicyCarbonData = CountryPolicyCarbonData('training_data.yaml', country, include_flags=False,
-                                                      policy_category=PolicyCategory.HEALTH_INDICATORS)
+                                                      policy_category=PolicyCategory.ALL,
+                                                      normalize=norm_data)
     train_x, train_y, test_x, test_y = countryPolicyCarbonData.split_train_test(fill_nan=True)
     train_features = train_features.append(train_x)
     test_features = test_features.append(test_x)
@@ -41,8 +43,10 @@ print(test_labels.shape)
 # Train model with 5 fold cross validation
 tss = TimeSeriesSplit()
 _, n_features = train_features.shape
-lstm = LSTM_2(training_config, num_features=n_features, num_outputs=1)
+lstm = DeepLearningModel(training_config, num_features=n_features, num_outputs=1)
 losses = []
+val_losses = []
+start = time.time()
 for train_idx, test_idx in tss.split(train_features):
     X, X_val = train_features.iloc[train_idx], train_features.iloc[test_idx]
     Y, Y_val = train_labels.iloc[train_idx], train_labels.iloc[test_idx]
@@ -50,6 +54,10 @@ for train_idx, test_idx in tss.split(train_features):
     val_f, val_l = utils.data_sequence_generator(X_val, Y_val, training_config['time_steps'])
     h = lstm.train_with_validation_provided(features, labels, val_f, val_l)
     losses.append(h.history['loss'])
+    val_losses.append(h.history['val_loss'])
+end = time.time()
+print("TRAINING TIME: {}".format(end - start))
+
 
 # Plot training loss
 loss_arr = np.zeros((50, 1))
@@ -57,8 +65,14 @@ for loss_per_fold in losses:
     for j, loss in enumerate(loss_per_fold):
         loss_arr[j] = loss_arr[j] + loss
 loss_arr = loss_arr / 5
+val_loss_arr = np.zeros((50, 1))
+for val_loss_per_fold in val_losses:
+    for j, loss in enumerate(val_loss_per_fold):
+        val_loss_arr[j] = val_loss_arr[j] + loss
+val_loss_arr = loss_arr / 5
 fig1, ax1 = plt.subplots()
 ax1.plot(range(len(loss_arr)), loss_arr, label='Training Loss')
+ax1.plot(range(len(val_loss_arr)), val_loss_arr, label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend(loc='upper right')
@@ -66,7 +80,10 @@ plt.title('Loss')
 plt.show()
 
 # # Prediction
+test_start = time.time()
 test_f, test_l = utils.data_sequence_generator(test_features, test_labels, training_config['time_steps'])
 model_eval = lstm.model.evaluate(test_f, test_l)
+test_end = time.time()
+print("TESTING TIME: {}".format(test_end - test_start))
 print("\n\nTesting Loss: {}\nTesting Accuracy: {}".format(model_eval[0], model_eval[1]))
 lstm.save("LSTM_TAPAN")

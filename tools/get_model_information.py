4a2ea00a445cd44e7822f8efac6af4e9681e71f1
Sreetama Sarkar <sreetama1994@gmail.com>
2020-08-14 01:26:56 +0200

    Added DNN for hyperparameter calculation

--- tools/get_model_information.py
+++ tools/get_model_information.py
@@ -6,6 +6,7 @@ Date: 13/08/20
 import json
 from tensorflow import keras
 from predictCO2.models.deep_learning_model import DeepLearningModel
+from predictCO2.models.mixed_nn_model import nn_model
 
 # 1. LSTM_TAPAN
 with open('cfg/lstm_config.json') as f:
@@ -43,6 +44,20 @@ lstm = DeepLearningModel(lstm_config, num_features=17, num_outputs=1)
 lstm.load()
 opti = lstm.model.optimizer
 print("CNN_TAPAN\n\n")
+print("Learning Rate: {}\n".format(keras.backend.eval(opti.lr)))
+print("Beta 1: {}\n".format(keras.backend.eval(opti.beta_1)))
+print("Beta 2: {}\n".format(keras.backend.eval(opti.beta_2)))
+print("Epsilon: {}\n".format(keras.backend.eval(opti.epsilon)))
+print("Amsgrad: {}\n\n\n".format(keras.backend.eval(opti.amsgrad)))
+
+# 4. DNN_SREETAMA
+with open('cfg/densenn_config.json') as f:
+    dnn_config = json.load(f)
+
+dnn = nn_model(dnn_config, num_features=8, num_outputs=1)
+dnn.load()
+opti = dnn.model.optimizer
+print("DNN_Sreetama\n\n")
 print("Learning Rate: {}\n".format(keras.backend.eval(opti.lr)))
 print("Beta 1: {}\n".format(keras.backend.eval(opti.beta_1)))
 print("Beta 2: {}\n".format(keras.backend.eval(opti.beta_2)))

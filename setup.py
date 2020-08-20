77d85fa10699df11a224b6cc4477a054107f930e
ga42hiz <joachim.jakob@tum.de>
2020-08-20 15:49:12 +0200

    edited setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     version="1.0",
-    description="Background Processing Server",
-    author="sebastian.lettner24@gmail.com",
+    description="Group7-AMI",
     install_requires=open("./requirements.txt").readline()
 )

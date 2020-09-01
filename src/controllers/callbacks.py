b83c9a1b2fd5706fb4d169ff1ef271d324f147a9
Tapan Sharma <01tapansharma@gmail.com>
2020-09-02 00:11:56 +0200

    Incorporating stringency callback into submit button callback

--- src/controllers/callbacks.py
+++ src/controllers/callbacks.py
@@ -2,15 +2,15 @@
 Created by: Tapan Sharma
 Date: 28/08/20
 """
-import json
-from dash.dependencies import Output, Input, State
+import dash
+import plotly.express as px
+from dash_extensions.enrich import State, Output, Input, Trigger
 
 from controllers.model_input_parser import ParseModelInputs, DataAnalysingModels
-from predictCO2.models.deep_learning_model import DeepLearningModel
 from controllers.model_output_generator import GenerateOutput
-import plotly.express as px
 
-def register_callbacks(app):
+
+def register_callbacks(app, dcc):
     @app.callback(
         Output(component_id='stringency_index_show', component_property='children'),
         [Input(component_id='stringency_index', component_property='value')]
@@ -37,7 +37,7 @@ def register_callbacks(app):
             return {'display': 'none'}
 
     @app.callback(
-        Output(component_id='output-mode-selector', component_property='children'),
+        Output(component_id='outputs', component_property='children'),
         [Input('submit_policy_selection', 'n_clicks')],
         [State('input-switch', 'value'),
          State('stringency_index', 'value'),
@@ -56,41 +56,27 @@ def register_callbacks(app):
                                  public_events_score, gathering_restrictions_score, public_transport_score,
                                  stay_home_score,
                                  internal_movement_score, international_travel_score, countries):
-        # 1. Parse Inputs in a required structure.
-        dataframe = None
-        if input_switcher_state:#when the toggle button is on right(stringency slider is on display)
-            parse_model_input = ParseModelInputs(DataAnalysingModels.STRINGENCY_INDEX_MODEL)
-            parse_model_input.stringency_idx = float(stringency_idx)
-            # TODO: @Sreetama add stringency model here.
+        if n_clicks > 0:
+            parse_model_input = ParseModelInputs()
+            parse_model_input.countries = countries
+            if input_switcher_state:  # when the toggle button is on right(stringency slider is on display)
+                parse_model_input.model_type = DataAnalysingModels.STRINGENCY_INDEX_MODEL
+                parse_model_input.stringency_idx = float(stringency_idx)
+            else:
+                parse_model_input.model_type = DataAnalysingModels.SOCIAL_POLICY_MODEL
+                parse_model_input.school_closing_score = int(school_closing_score)
+                parse_model_input.workspace_closing_score = int(workspace_closing_score)
+                parse_model_input.public_events_score = int(public_events_score)
+                parse_model_input.gathering_restrictions_score = int(gathering_restrictions_score)
+                parse_model_input.public_transport_score = int(public_transport_score)
+                parse_model_input.stay_home_score = int(stay_home_score)
+                parse_model_input.internal_movement_score = int(internal_movement_score)
+                parse_model_input.international_travel_score = int(international_travel_score)
+            out = GenerateOutput()
+            df = out.get_dataframe_for_plotting(parse_model_input, countries)
+            fig = px.line(df, x='date', y='co2', color='country')
+            fig.update_layout(transition_duration=500)
+            return "Processing Finished!", dcc.Graph(id='co2-graph', figure=fig), dcc.Store(id='trigger')
         else:
-            parse_model_input = ParseModelInputs(DataAnalysingModels.SOCIAL_POLICY_MODEL)
-            parse_model_input.school_closing_score = int(school_closing_score)
-            parse_model_input.workspace_closing_score = int(workspace_closing_score)
-            parse_model_input.public_events_score = int(public_events_score)
-            parse_model_input.gathering_restrictions_score = int(gathering_restrictions_score)
-            parse_model_input.public_transport_score = int(public_transport_score)
-            parse_model_input.stay_home_score = int(stay_home_score)
-            parse_model_input.internal_movement_score = int(internal_movement_score)
-            parse_model_input.international_travel_score = int(international_travel_score)
-            dataframe, _ = parse_model_input.get_social_policy_data()
+            return "Press submit when ready.", None, dcc.Store(id='trigger')
 
-        # print(dataframe.shape)
-        # with open('cfg/cnn_config.json') as f:
-        #     training_config = json.load(f)
-        # cnn = DeepLearningModel(training_config, num_features=8, num_outputs=1)
-        # cnn.load()
-        # y = cnn.model.predict(dataframe)
-        # print(y)
-
-    @app.callback(
-        Output('co2-graph', 'figure'),
-        [Input('submit_policy_selection', 'n_clicks')],
-        [State('stringency_index', 'value'),
-         State('country-dropdown', 'value')])
-    def update_figure(n_clicks, str_index, countries):
-        out = GenerateOutput()
-        df = out.get_dataframe_for_plotting(str_index, countries)
-        fig = px.line(df, x='date', y='co2', color='country')
-
-        fig.update_layout(transition_duration=500)
-        return fig

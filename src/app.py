2b36f5e9ce62b9b646d78e254b46bdccf458849b
Saptarshi <saptarshi14mitra@gmail.com>
2020-09-03 17:24:02 +0200

    added card for div for map

--- src/app.py
+++ src/app.py
@@ -282,9 +282,13 @@ app.layout = html.Div(
                                   ),
                                   html.Div(className='right',
                                            children=[
-                                                dcc.Graph(id="my-graph", figure=fig)
-                                                # dcc.Graph(id='co2-percent-graph', figure=fig)
-                                                #html.Img(src='assets/map.png', height=400, width=700)
+                                               html.Div(className='map-container map-container-1',
+                                                        children=[
+                                                            dcc.Graph(id="my-graph", figure=fig)
+                                                            # dcc.Graph(id='co2-percent-graph', figure=fig)
+                                                            #html.Img(src='assets/map.png', height=400, width=700)
+                                                        ]
+                                               ),
                                            ]
                                   )
                                   #html.Img(src='assets/map.png', height=400, width=700)

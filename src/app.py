2195884291a06928fe27405f5d3315880dd06dc1
Saptarshi <saptarshi14mitra@gmail.com>
2020-09-03 03:00:38 +0200

    uncommented the dummy interactive map

--- src/app.py
+++ src/app.py
@@ -22,38 +22,38 @@ from dash_extensions.enrich import Dash
 import pickle
 import plotly.graph_objects as go
 
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
-# fig = go.Figure(data=go.Choropleth(
-#     locations = df['CODE'],
-#     z = df['GDP (BILLIONS)'],
-#     text = df['COUNTRY'],
-#     colorscale = 'Blues',
-#     autocolorscale=False,
-#     reversescale=True,
-#     marker_line_color='darkgray',
-#     marker_line_width=0.5,
-#     colorbar_tickprefix = '$',
-#     colorbar_title = 'GDP<br>Billions US$',
-# ))
-#
-# fig.update_layout(
-#     title_text='2014 Global GDP',
-#     autosize=True,
-#     geo=dict(
-#         showframe=False,
-#         showcoastlines=False,
-#         projection_type='equirectangular'
-#     ),
-#     annotations = [dict(
-#         x=0.55,
-#         y=0.1,
-#         xref='paper',
-#         yref='paper',
-#         text='Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">\
-#             CIA World Factbook</a>',
-#         showarrow = False
-#     )]
-# )
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+fig = go.Figure(data=go.Choropleth(
+    locations = df['CODE'],
+    z = df['GDP (BILLIONS)'],
+    text = df['COUNTRY'],
+    colorscale = 'Blues',
+    autocolorscale=False,
+    reversescale=True,
+    marker_line_color='darkgray',
+    marker_line_width=0.5,
+    colorbar_tickprefix = '$',
+    colorbar_title = 'GDP<br>Billions US$',
+))
+
+fig.update_layout(
+    title_text='2014 Global GDP',
+    autosize=True,
+    geo=dict(
+        showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    ),
+    annotations = [dict(
+        x=0.55,
+        y=0.1,
+        xref='paper',
+        yref='paper',
+        text='Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">\
+            CIA World Factbook</a>',
+        showarrow = False
+    )]
+)
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 external_stylesheets = [
     "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"]
@@ -279,9 +279,9 @@ app.layout = html.Div(
                                   ),
                                   html.Div(className='right',
                                            children=[
-                                                #dcc.Graph(id="my-graph", figure=fig)
+                                                dcc.Graph(id="my-graph", figure=fig)
                                                 # dcc.Graph(id='co2-percent-graph', figure=fig)
-                                                html.Img(src='assets/map.png', height=400, width=700)
+                                                #html.Img(src='assets/map.png', height=400, width=700)
                                            ]
                                   )
                                   #html.Img(src='assets/map.png', height=400, width=700)
